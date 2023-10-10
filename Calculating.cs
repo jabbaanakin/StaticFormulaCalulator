0e6726c36f27057849861da40869e61ba5b6c020
jabbaanakin <oganisyan.yakob@mail.ru>
2023-10-10 16:56:44 +0300

    classes added

---
+++ Calculating.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StaticFormulaCalulator
+{
+    /// <summary>
+    /// Класс, предназначенный для рассчета готовой формулы
+    /// </summary>
+    class Calculating
+    {
+
+        public Calculating()
+        {
+
+        }
+
+        /// <summary>
+        /// Метод, который отправляет сначала операции в скобках,
+        /// пока все скобки не будут открыты
+        /// </summary>
+        /// <param name="f">string Формула</param>
+        public decimal Calculate(ref string formula)
+        {
+            bool deletable = true;
+
+            //Пока все скобки не будут раскрыты, отправляем операции в скобках на решение
+            while (deletable)
+            {
+                string ff = formula;
+                if (ff.IndexOf(')') != -1)
+                {
+                    string[] rfs = ff.Split(')');
+                    string[] lfs = rfs[0].Split('(');
+
+                    string temp = lfs[^1];
+
+                    string t = CalculateEachValue(temp).ToString();
+
+                    formula = formula.Replace(String.Concat("(", temp, ")"), t);
+                }
+                else
+                {
+                    deletable = false;
+                }
+            }
+
+            //Итоговый расчет формулы с округлением до двух знаков после запятой
+            return Math.Round(CalculateEachValue(formula), 2, MidpointRounding.AwayFromZero);
+        }
+
+        /// <summary>
+        /// Рассчитывает формулу
+        /// </summary>
+        /// <param name="bform">string формула(либо полная, либо в скобках)</param>
+        /// <returns>decimal result</returns>
+        private decimal CalculateEachValue(string bform)
+        {
+            Regex regHighProir = new Regex(@"\*|\/");
+
+            Regex regLowProir = new Regex(@"\+|\-");
+            string[] lowVals = regLowProir.Split(bform);
+
+            MatchCollection lows = regLowProir.Matches(bform);
+            MatchCollection highs = regHighProir.Matches(bform);
+
+            if (highs.Count > 0 || lows.Count > 0)
+            {
+                //Сначала рассчитываем операции умножения или деления (высокий приоритет)
+                for (int i = 0; i < lowVals.Length; i++)
+                {
+                    if (regHighProir.IsMatch(lowVals[i]))
+                    {
+                        decimal result = 0m;
+                        //Разделяем формулу на под формулы благодаря менее приоритетным операциям +, -
+                        // Предположим у нас формула x-a*b, когда мы разделим по low priority operation (смотря какой fabric)
+                        //то получим x, a*b. И в данном блоке как раз и происходит умножение
+                        MatchCollection ms = regHighProir.Matches(lowVals[i]);
+                        while (ms.Count > 0)
+                        {
+                            lowVals = regLowProir.Split(bform);
+                            string[] hh = regHighProir.Split(lowVals[i]);
+                            ms = regHighProir.Matches(lowVals[i]);
+                            if (ms.Count <= 0)
+                                break;
+                            switch (ms[0].Value)
+                            {
+                                case "*":
+                                    {
+                                        result = Convert.ToDecimal(hh[0]) * Convert.ToDecimal(hh[1]);
+                                        break;
+                                    }
+                                case "/":
+                                    {
+                                        result = Convert.ToDecimal(hh[0]) / Convert.ToDecimal(hh[1]);
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        break;
+                                    }
+                            }
+
+                            bform = bform.Replace(String.Concat(hh[0], ms[0].Value, hh[1]), result.ToString());
+                        }
+                    }
+                }
+                decimal resulth = 1;
+                lows = regLowProir.Matches(bform);
+                while (lows.Count > 0)
+                {
+                    //После того, как рассчитаны операции с высоким приориетом, мы просчитываем операции с низким приоритетом
+                    //В случае, если результат у нас выходит отрицательным (Например, -3,14), то выдаем ошибку, что данные неверны
+                    //Так как в случае с деньгами и услугами, компания ничего не должна клиенту денег
+                    lowVals = regLowProir.Split(bform);
+                    lows = regLowProir.Matches(bform);
+                    if (lows.Count <= 0)
+                        break;
+                    switch (lows[0].Value)
+                    {
+                        case "+":
+                            {
+                                resulth = Convert.ToDecimal(lowVals[0]) + Convert.ToDecimal(lowVals[1]);
+                                break;
+                            }
+                        case "-":
+                            {
+                                resulth = Convert.ToDecimal(lowVals[0]) - Convert.ToDecimal(lowVals[1]);
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+                    if (resulth <= 0)
+                    {
+                        Console.WriteLine("Во время расчета число получилось отрицательным. Для избежания ошибки просьба проверить данные.\n" +
+                            $"Ошибка произошла при следующей операции: {lowVals[0]} {lows[0].Value} {lowVals[1]} = {resulth}.\n" +
+                            $"Для дальнейшего расчета результат операции изменен на 1");
+                        resulth = 1;
+                    }
+                    bform = bform.Replace(String.Concat(lowVals[0], lows[0].Value, lowVals[1]), resulth.ToString());
+                }
+            }
+            return Convert.ToDecimal(bform);
+        }
+    }
+}
---
+++ ParserForStaticFormula.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StaticFormulaCalulator
+{
+    /// <summary>
+    /// Класс, предназначенный для того, чтобы поправить формулу для клиента, а также
+    /// сгенерировать формулу с подставленными значениями стоимости
+    /// </summary>
+    internal class ParserForStaticFormula
+    {
+        public ParserForStaticFormula()
+        {
+
+        }
+
+        /// <summary>
+        /// Конструктор класса, соответственно в этом методе поправляется формула для клиента, а также 
+        /// генерируется формула с подставленными значениями стоимости
+        /// </summary>
+        /// <param name="f">String формула</param>
+        /// <param name="dt">DataTable таблица с набором компонентов</param>
+        /// <returns>Записывает в свойства класса формулу для клиента и формулу для дальнейшего рассчета</returns>
+        public string ParseForStaticFormula(ref string f, DataTable dt)
+        {
+            string formula = f; //Формула, в которую подставим числовые значения
+
+            string[] variables = dt.AsEnumerable().Select(row => row.Field<string>("ShortName")!).ToArray();
+
+            //Разделяем формулу на неизвестные компоненты и удаляем из формулы компоненты, 
+            //которые не были выбраны клиентом
+            //!!!ВАЖНО!!! В формуле не должно быть пробелов
+            Regex regSplit = new Regex(@"\W");
+            string[] result = regSplit.Split(formula);
+            foreach (string res in result)
+            {
+                if (!variables.Contains(res) && res != "")
+                {
+                    DeleteExcessComponents(res, formula);
+                    DeleteExcessComponents(res, f);
+                }
+            }
+
+            //В этом блоке мы как раз и заменяем краткое название компонентов на полное
+            List<string[]> variableArray = new List<string[]>();
+            var groups = variables.GroupBy(v => v);
+            foreach (var group in groups)
+            {
+                variableArray.Add(group.ToArray());
+            }
+
+            BuildCorrectFormulas(variableArray, dt, ref f,  1); 
+            BuildCorrectFormulas(variableArray, dt, ref formula, 4);
+
+            //Переставляем компоненты на значения для второй формулы, а в дальнейшем и для просчета
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                formula = formula.Replace((string)dt.Rows[i][4], dt.Rows[i][2].ToString());
+            }
+
+            return formula;
+        }
+
+        /// <summary>
+        /// Удаляет из формулы недостающие компоненты
+        /// </summary>
+        /// <param name="variables">string[] Массив компонентов</param>
+        /// <param name="formula">string Формула</param>
+        /// <returns>В будущем можно слегка переписать, чтобы возвращало строку либо две строки в string[]</returns>
+        public void DeleteExcessComponents(string res, string formula)
+        {
+            if (formula[formula.IndexOf(res) - 1] == '(' && formula[formula.IndexOf(res) + 2] == ')')
+            {
+                formula = formula.Remove(formula.IndexOf(res) - 2, res.Length + 3);
+            }
+            else if (formula[formula.IndexOf(res) - 1] == '(')
+            {
+                formula = formula.Remove(formula.IndexOf(res), res.Length + 1);
+            }
+            else
+            {
+                formula = formula.Remove(formula.IndexOf(res) - 1, res.Length + 1);
+            }
+        }
+
+        /// <summary>
+        /// Заменяет в формуле краткие значения на полные, а также заменяет компоненты на их значения
+        /// </summary>
+        /// <param name="variables">string[] Массив компонентов</param>
+        /// <param name="dt">DataTable Таблица с данными</param>
+        /// <param name="formula">DataTable Формула</param>
+        /// <param name="index">int Колонка, с которой стоит брать значения</param>
+        /// <returns>Возвращает готовую формулу с значениями</returns>
+        public void BuildCorrectFormulas(List<string[]> variableArray, DataTable dt, ref string formula, int index)
+        {
+            foreach (string[] s in variableArray.ToArray())
+            {
+                DataRow[] dr = dt.Select($"ShortName = '{s[0]}'");
+                if (s.Length > 1)
+                {
+                    string replacingVarible = "(" + (string)dr[0][index];
+                    for (int i = 1; i < s.Length; i++)
+                    {
+                        replacingVarible += "+" + (string)dr[i][index];
+                    }
+                    replacingVarible += ")";
+                    Regex regularEx = new Regex($"{s[0]}+");
+                    if (regularEx.IsMatch(formula))
+                    {
+                        formula = formula.Replace(s[0], replacingVarible);
+                    }
+                }
+                else if(index == 4)
+                {
+                    string replacingVaribleFull = (string)dr[0][index];
+                    formula = formula.Replace(s[0], replacingVaribleFull);
+                }
+            }
+        }
+
+    }
+}
